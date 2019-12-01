950081b9c1326b46d8c3ff45963fa0c5216f7e38
jrg96 <gomezlopez.jorge96@gmail.com>
2019-12-01 09:57:44 -0800

    Cambio de titulo barra navegacion

--- application/views/templates/login.php
+++ application/views/templates/login.php
@@ -18,7 +18,7 @@
 
 <body>
     <nav class="navbar navbar-light navbar-expand-md">
-        <div class="container-fluid"><a class="navbar-brand" href="#"><strong>Rivas y Gonzalez</strong></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+        <div class="container-fluid"><a class="navbar-brand" href="#"><strong>Bufete Ciber Abogados</strong></a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div
                 class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav ml-auto">
