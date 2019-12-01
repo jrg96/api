b31cd3f29bf8c3021b3a7495219a03b0e89e0f83
jrg96 <gomezlopez.jorge96@gmail.com>
2019-12-01 09:54:48 -0800

    Cambio titulo login

--- application/views/templates/login.php
+++ application/views/templates/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rivas y Gonzalez</title>
+    <title>Bufete Ciber Abogados</title>
     <link rel="stylesheet" href="{$base_url}assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="{$base_url}assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="{$base_url}assets/fonts/material-icons.min.css">
